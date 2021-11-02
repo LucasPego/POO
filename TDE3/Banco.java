6b2e355d45db8a45a4db369f418287e8e94b557c
LucasPego <80799463+LucasPego@users.noreply.github.com>
2021-11-01 21:09:04 -0300

    Update Banco.java

--- TDE3/Banco.java
+++ TDE3/Banco.java
@@ -1,83 +1,34 @@
 package TDE3;
 
 import java.util.ArrayList;
-import java.util.Scanner;
 
 public class Banco {
+    private String nome;
+    private ArrayList<Cliente> clientes;
 
-	private String nome;
-	private ArrayList<Cliente> clientes;
+    public Banco(String nome) {
+        this.nome = nome;
+        this.clientes   = new ArrayList<Cliente> ();
+    }
+    public void addCliente(Cliente c) {  
+        clientes.add(c);
+    }
+    public static void main(String[] args) {
+        Banco banco = new Banco("MeuBanco&Cia.");
 
-	// Construtor
-	public Banco(String nome) {
-		this.nome = nome;
-		this.clientes = new ArrayList<Cliente>(); // Lista de clientes
-	}
+        banco.addCliente  (new Cliente ("Lucas", 200));
+        banco.addCliente  (new Cliente ("Gabriel",  201));
+        banco.addCliente  (new Cliente ("Guilherme",  202));
 
-	public void addCliente(Cliente c) {
-		clientes.add(c);
-	}
+        System.out.println("Banco " + banco.nome + " - Operações");
+        for (Cliente cliente : banco.clientes) {//Iterador
+            System.out.println("\nCliente " + cliente.getNome() + " - operações");
+            cliente.opera();
+        }
 
-	public static void menuPrincipal() {
-		System.out.println("Menu de Opções :");
-		System.out.println("1 : Cadastrar Cliente");
-		System.out.println("2 : Consultar Cliente");
-		System.out.println("3 : Listar Clientes");
-		System.out.println("4 : Sacar");
-		System.out.println("5 : Depositar");
-		System.out.println("6 : Sair");
-	}
-
-	public static void main(String[] args) {
-		Banco banco = new Banco("Banco do Lucas Pego");
-		Scanner leitor = new Scanner(System.in);
-		int opcao = 5;
-
-		while (true) {
-			menuPrincipal();
-			opcao = Integer.parseInt(leitor.nextLine());
-
-			switch (opcao) {
-			case 1: {
-				String nome;
-				int numeroConta;
-				System.out.println("Informe o nome ");
-				nome = leitor.nextLine();
-				System.out.println("Informe o número da conta");
-				numeroConta = Integer.parseInt(leitor.nextLine());
-				Cliente cliente = new Cliente(nome, numeroConta);
-				banco.addCliente(cliente);
-				break;
-			}
-			case 2: {
-				System.out.println("Informe a conta corrente");
-				int conta = Integer.parseInt(leitor.nextLine());
-				Cliente clienteAchado = null;
-				for (Cliente clienteBusca : banco.clientes) {
-					if (clienteBusca.getConta().getNumero() == conta) {
-						clienteAchado = clienteBusca;
-						break;
-					}
-				}
-				if (clienteAchado != null) {
-					clienteAchado.getConta().imprimeConta();
-				} else {
-					System.out.println();
-				}
-
-				break;
-
-			}
-
-			case 5: {
-				System.out.println("Programa Encerrado");
-				System.exit(0);
-			}
-			default: {
-				System.out.println("Opção não existe");
-			}
-			}
-		}
-	}
-
+        System.out.println("\nBanco " + banco.nome + "- Clientes");
+        for (Cliente cliente: banco.clientes) { //Iterador
+            cliente.getConta().imprimeConta();
+        }
+    }
 }
